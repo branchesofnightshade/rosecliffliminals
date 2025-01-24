eb5479542df01eebfc4f4b8f0fb5035ff3d8d555
rosybnightshade <oliviaschuetz11@gmail.com>
2025-01-24 13:32:18 -0700

    start the switch

--- main/error.js
+++ main/error.js
@@ -1,1 +1,32 @@
 let pass = document.getElementById('password');
+let user = document.getElementById('user');
+
+pass.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        passwordBypass();
+    }
+    else {
+        console.log("irrelevant key press");
+    }
+});
+
+function passwordBypass() { 
+    
+    const wait = document.getElementById('wait');
+    const permission = document.getElementById('permission');
+
+    switch (pass.value) {
+        case '211605':
+        console.log(pass.value);
+        wait.innerHTML +=  `
+        <span>Please Wait...</span>
+        `
+        permission.innerHTML += `
+        <span>Access Granted</span>
+        `
+        break;
+
+        default:
+        console.log("mb chat");
+    }
+}
