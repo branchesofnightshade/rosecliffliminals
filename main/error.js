66afc3ba89e0092731b0ce26ccd196611c591982
Olivia (Via/Vi/V) Schuetz <oliviaschuetz11@gmail.com>
2025-02-12 03:13:50 +0000

    terminal not yet activated

--- main/error.js
+++ main/error.js
@@ -1,7 +1,10 @@
-let pass = document.getElementById('password');
-let user = document.getElementById('user');
+import {ft} from "../directory/taylor.js";
+import {eb} from "../directory/bridger.js";
 
-pass.addEventListener("keypress", function(event) {
+export let pass = document.getElementById('password');
+export let user = document.getElementById('user');
+
+pass.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
         passwordBypass();
     }
@@ -19,53 +22,15 @@ function passwordBypass() {
 
     switch (pass.value) {
         case '211605':
-            console.log('Password entered: ' + pass.value);
-            wait.textContent = "Please Wait...";
-            setTimeout(() => {permission.textContent = "Access Granted";},2000);
-            setTimeout(() => {
-                let user = "E.Bridger";
-                pass.style.display = "none";
-                welcome.innerHTML += `Welcome Back ${user}`
-                hmm.innerHTML += `<b>phoenixfeather@saveme: ~$</b><input type="text" id="think"> `
-
-                const think = document.getElementById('think');
-
-                think.addEventListener("keypress", function(event) {
-                    if (event.key === "Enter") {
-                        switch (think.value) {
-                            case 'recipes':
-                                let none = document.getElementById('none');
-                                none.innerHTML += `https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/JSON`;
-                                break;
-
-                            default:
-                                console.log("nope");
-                        }
-                    } else {
-                        console.log("irrelevant key press");
-                    }
-                    // think.value = '';
-                });
-            },5000);
-            break;
-
+           eb();
+           break;
         case '412184':
-            fetch('/filesystem.json')
-            .then(response => response.json())
-            .then(users => {
-                
-            })
-            .catch(error => console.log(error));
+            ft();
             break;
-
         default:
             console.log("Invalid password attempt");
             wait.textContent = "Please Wait...";
             setTimeout(() => {permission.textContent = "Access Denied";},6000);
     }
     pass.value = '';
-}
-
-function typeError() {
-
 }
